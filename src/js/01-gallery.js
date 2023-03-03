a4ef13544f7716a5ac5c25b59c75ab440f4bd900
6teendollars <91662992+6teendollars@users.noreply.github.com>
2023-03-03 12:06:15 +0100

    task-1
    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+
+
+const galleryRef = document.querySelector(".gallery"); // выбираем елемент галереи
+galleryRef.innerHTML = createMarkup(galleryItems); // созданиу html разметки  которая принимает массив объектов galleryItems и возвращает строку с HTML-разметкой для каждого изображения в массиве.
+
+function createMarkup(items) {
+  return items
+    .map((e) => {
+      //перебирает массив, генерируя HTML-строку для каждого объекта.
+      return `<div><a class="gallery__item" href="${e.original}">  
+  <img class="gallery__image" src="${e.preview}" alt="${e.description}"/>
+</a></div>`;
+    })
+    .join(""); // обеденяеться в одну строку
+}
+
+let gallery = new SimpleLightbox(".gallery a", { //создаем екземпляр библиотеки SimpleLightbox
+  //селектор для ссылок галереи (".gallery a")
+  captionsData: "alt", //объект с параметрами , подпись
+  captionDelay: 200, // дилей для появления подписи
+});
+gallery.on("show.simplelightbox"); //прослушивание слбытия, которое срабатывает при отображении изображения в галерее
