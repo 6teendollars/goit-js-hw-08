e2b57e1ff5db9aa4cc87be024f87176518f4d9eb
6teendollars <91662992+6teendollars@users.noreply.github.com>
2023-03-04 11:34:50 +0100

    task-2
    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,26 @@
-import Player from '@vimeo/player'
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
+console.log(player);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+    // player.on('play', function() {
+    //     console.log('played the video!');
+    // });
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+    // player.getVideoTitle().then(function(title) {
+    //     console.log('title:', title);
+    // });
+
+    function onProgress({seconds}){
+        localStorage.setItem("videoplayer-current-time", seconds);
+    }
+
+    player.on('timeupdate', throttle(onProgress, 1000));
+
+    const storedTime = localStorage.getItem("videoplayer-current-time");
+
+    if(storedTime){
+        player.setCurrentTime(storedTime)
+    }
