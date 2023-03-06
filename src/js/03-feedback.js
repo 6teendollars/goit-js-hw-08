483451656e740227ca3bcf77398ba9e8a0281a18
6teendollars <91662992+6teendollars@users.noreply.github.com>
2023-03-06 21:28:53 +0100

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,26 +3,26 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 const inputEmail = document.querySelector('input[name="email"]');
 const message = document.querySelector('textarea[name="message"]');
-const feedbackInfo = {};
+const feedback = {};
 
 form.addEventListener('input', throttle(getInputValues, 500));
 form.addEventListener('submit', handleSubmit);
 
 try {
-  const currentFeedbackInfo = JSON.parse(
+  const currentFeedback = JSON.parse(
     localStorage.getItem('feedback-form-state')
   );
-  inputEmail.value = currentFeedbackInfo.email ? currentFeedbackInfo.email : '';
-  message.value = currentFeedbackInfo.message
-    ? currentFeedbackInfo.message
+  inputEmail.value = currentFeedback.email ? currentFeedback.email : '';
+  message.value = currentFeedback.message
+    ? currentFeedback.message
     : '';
 } catch (error) {
   console.log(error.message);
 }
 
 function getInputValues(event) {
-  feedbackInfo[`${event.target.name}`] = event.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(feedbackInfo));
+  feedback[`${event.target.name}`] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(feedback));
 }
 
 function handleSubmit(event) {
